9b88486d37e5cc9e24b830294f2b79d47358a2a9
bishoy basily <bishoybasily@gmail.com>
2019-08-12 19:15:22 +0200

    formatting

--- crd/src/main/java/com/gmail/bishoybasily/k8s/crd/fidelyo/FidelyoList.java
+++ crd/src/main/java/com/gmail/bishoybasily/k8s/crd/fidelyo/FidelyoList.java
@@ -2,8 +2,8 @@ package com.gmail.bishoybasily.k8s.crd.fidelyo;
 
 import io.fabric8.kubernetes.client.CustomResourceList;
 
-// the list, which will be returned for operations like get,
-// it contains a method called get list will should return an iterable of the Fidelyo type available in the cluster,
+// the list, which will be returned as a result for operations like get fidelyos,
+// it contains a method called getList which should return an iterable of the Fidelyos available in the cluster,
 // it also holds some extra information about the list itself
 public class FidelyoList extends CustomResourceList<Fidelyo> {
 
